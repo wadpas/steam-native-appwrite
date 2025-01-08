0f9528c761d9d98d5f0ae193ceb1b7c0df1b282c
wadpas <wadpas@gmail.com>
2025-01-08 02:04:56 +0200

    setting fonts & colors

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,7 +5,8 @@ import { Link } from 'expo-router'
 const Index = () => {
   return (
     <View>
-      <Text>Index</Text>
+      <Text className='bg-slate-300 font-rubik'>WIndex</Text>
+      <Text className='bg-primary-300 '>WIndex</Text>
       <Link href='/sign-in'>SignIn</Link>
       <Link href='/properties/1'>Props</Link>
     </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,27 +6,31 @@ import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
 
+import '../assets/global.css'
 import { useColorScheme } from '@/hooks/useColorScheme'
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [fontsLoaded] = useFonts({
+    'Rubik-Bold': require('../assets/fonts/Rubik-Bold.ttf'),
+    'Rubik-ExtraBold': require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    'Rubik-Light': require('../assets/fonts/Rubik-Light.ttf'),
+    'Rubik-Medium': require('../assets/fonts/Rubik-Medium.ttf'),
+    'Rubik-Regular': require('../assets/fonts/Rubik-Regular.ttf'),
+    'Rubik-SemiBold': require('../assets/fonts/Rubik-SemiBold.ttf'),
   })
 
   useEffect(() => {
-    if (loaded) {
+    if (fontsLoaded) {
       SplashScreen.hideAsync()
     }
-  }, [loaded])
+  }, [fontsLoaded])
 
-  if (!loaded) {
+  if (!fontsLoaded) {
     return null
   }
-
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
